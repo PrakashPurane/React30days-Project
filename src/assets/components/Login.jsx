1d001cdd1475ea887921654e811b9df9bf348919
PrakashPurane <chhanprakash@gmail.com>
2024-04-20 22:51:56 -0700

    integrated register and login

--- src/assets/components/Login.jsx
+++ src/assets/components/Login.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import Form from './components2/form/Form'
+import React from "react";
+import Form from "./components2/form/Form";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const Login = () => {
+const Login = (data) => {
+  const navigate = useNavigate();
+  const handleLogin = async (data) => {
+    try {
+      const response = await axios.post(
+        "https://react30.onrender.com/api/user/login",
+        data
+      );
+      if (response.status === 200) {
+        navigate("/");
+      } else {
+        alert("Login failed");
+      }
+    } catch (error) {
+      alert("error?.response?.data?.message");
+    }
+  };
+
   return (
     <>
-  <Form type='Register'/>
+      <Form type="Login" onSubmit={handleLogin} />
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/assets/components/Register.jsx
+++ src/assets/components/Register.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Form from './components2/form/Form'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+  const navigate = useNavigate()
+  const handleRegister= async(data)=>{
+   try {
+    const response= await axios.post("https://react30.onrender.com/api/user/register", data)
+    if (response.status === 201){
+      navigate('/login')
+    }
+      else{
+        alert("Registration failed")
+      }
+
+   } catch (error) {
+    alert("error?.response?.data?.message")
+   }
+    }
+
+
   return (
     <>
-  <Form type='Login'/>
+  <Form type='Register' onSubmit={handleRegister}/>
     </>
   )
 }
